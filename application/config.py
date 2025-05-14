055e5392bc5bb544f534371055250cf2a17e1d89
naili <nail.isntsnail@mail.ru>
2025-05-14 20:28:28 +0300

    requirements.txt fields addition for installments.

--- application/config.py
+++ application/config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config(object):
-    USER = os.environ.get("POSTGRES_USER", "venik")
-    PASSWORD = os.environ.get("POSTGRES_PASSWORD", "venik0910")
-    HOST = os.environ.get("POSTGRES_HOST", "localhost")
-    PORT = os.environ.get("POSTGRES_PORT", "5432")
-    DATABASE = os.environ.get("POSTGRES_DB", "education-DB")
+    USER = os.environ.get("POSTGRES_USER")
+    PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    HOST = os.environ.get("POSTGRES_HOST")
+    PORT = os.environ.get("POSTGRES_PORT")
+    DATABASE = os.environ.get("POSTGRES_DB")
 
     SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
     SECRET_KEY = '1923hnsdcvs09erkjgmdsq'
